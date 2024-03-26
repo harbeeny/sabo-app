31189c770a22ba4ef88a7817c7017e0ba9add030
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-03-26 12:37:54 -0400

    Implemented Card Containers
    working on side bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,6 +94,104 @@ export default function Home() {
               </div>
             </CardBody>
           </CardContainer>
+
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                Pro 
+                <h2 className="text-6xl">$29.99</h2>
+              </CardItem>
+
+              <CardItem
+                translateZ="60"
+                className="text-sm max-w-sm text-neutral-500 dark:text-neutral-300 mt-2"
+              >
+                Get a glimpse of what our software is capable of. Just a heads up {"you'll"} never leave us after this!
+                <ul className="my-4 flex flex-col gap-2">
+                  <li className="flex items-center gap-2">
+                    <CheckIcon />
+                    3 Free automations
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <CheckIcon />
+                    100 tasks per month
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <CheckIcon /> 
+                    Two-step Actions 
+                  </li>
+                </ul>
+              </CardItem>
+              <div className="flex justify-between items-center mt-8">
+                <CardItem 
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  Try now •
+                </CardItem>
+                <CardItem 
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black text-xs font-bold dark:bg-white dark:text-black text-white"
+                >
+                  Get Started Now
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                Unlimited 
+                <h2 className="text-6xl">$99</h2>
+              </CardItem>
+
+              <CardItem
+                translateZ="60"
+                className="text-sm max-w-sm text-neutral-500 dark:text-neutral-300 mt-2"
+              >
+                Get a glimpse of what our software is capable of. Just a heads up {"you'll"} never leave us after this!
+                <ul className="my-4 flex flex-col gap-2">
+                  <li className="flex items-center gap-2">
+                    <CheckIcon />
+                    3 Free automations
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <CheckIcon />
+                    100 tasks per month
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <CheckIcon /> 
+                    Two-step Actions 
+                  </li>
+                </ul>
+              </CardItem>
+              <div className="flex justify-between items-center mt-8">
+                <CardItem 
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  Try now •
+                </CardItem>
+                <CardItem 
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black text-xs font-bold dark:bg-white dark:text-black text-white"
+                >
+                  Get Started Now
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
         </div>
       </section> 
     </main>
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+type Props = {}
+
+const MenuOptions = (props: Props) => {
+  return (
+    <div>MenuOptions</div>
+  )
+}
+
+export default MenuOptions
